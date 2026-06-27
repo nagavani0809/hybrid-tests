72b877d563d8022593d49bc0bffa911b2d32bdc2
nagavani0809 <vanismart@gmail.com>
2026-06-27 12:05:00 +0530

    changed seluinuim file for chrome error

--- Test/selenium_tests/test_ui_sample.py
+++ Test/selenium_tests/test_ui_sample.py
@@ -1,8 +1,12 @@
-# tests/selenium_tests/test_ui_sample.py
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def test_google_title():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.google.com")
     assert "Google" in driver.title
     driver.quit()
